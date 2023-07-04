421e9b837b77344d92dedff365801ee25b2c9f4c
Dallibo <denismalyarenko1@gmail.com>
2023-07-04 21:52:22 +0300

    task 1

---
+++ check_js_6.js
@@ -0,0 +1,14 @@
+function numbers(numb1, numb2) {
+    if (numb1 < numb2) {
+        console.log(-1)
+    }
+    else if (numb1 > numb2) {
+        console.log(1)
+    }
+    else {
+        console.log(0)
+    }
+}
+let numb1 = 44
+let numb2 = 32
+numbers(numb1, numb2)
