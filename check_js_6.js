ad37ee139ca2e2a6ebf4d50dcd3b44e4f1ae9df4
Dallibo <denismalyarenko1@gmail.com>
2023-07-04 22:03:46 +0300

    task 2

--- check_js_6.js
+++ check_js_6.js
@@ -12,3 +12,13 @@ function numbers(numb1, numb2) {
 let numb1 = 44
 let numb2 = 32
 numbers(numb1, numb2)
+
+function factorial(number) {
+    let newFactorial = 1;
+    for (let i = 1; i <= number; i ++) {
+        newFactorial *= i;
+    }
+    return newFactorial
+}
+let number = 5
+console.log(factorial(number));
